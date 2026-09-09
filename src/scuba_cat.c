4e2909f85c217098a64be2e1b8e5e9e5e9aec5e1
RishabhSk7 <2002tej@gmail.com>
2026-09-09 22:02:48 +0530

    debugging scuba cat

--- src/scuba_cat.c
+++ src/scuba_cat.c
@@ -6,9 +6,20 @@ lv_obj_t *zmk_display_status_screen(void)
     lv_obj_t *screen = lv_obj_create(NULL);
 
     lv_obj_remove_style_all(screen);
+
     lv_obj_set_size(screen, 128, 32);
-    lv_obj_set_style_bg_color(screen, lv_color_white(), LV_PART_MAIN);
-    lv_obj_set_style_bg_opa(screen, LV_OPA_COVER, LV_PART_MAIN);
+
+    lv_obj_set_style_bg_color(
+        screen,
+        lv_color_white(),
+        LV_PART_MAIN
+    );
+
+    lv_obj_set_style_bg_opa(
+        screen,
+        LV_OPA_COVER,
+        LV_PART_MAIN
+    );
 
     return screen;
 }
